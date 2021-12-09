39ea96cd9e7c2acf4773aa8d99f4e987ef20f9b0
CarloGrigioni <carlogrigioni@gmail.com>
2021-12-09 20:50:16 +0100

    main js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./sw.js');
+    navigator.serviceWorker.register('/sw.js');
   }
 }
   let deferredPrompt;
