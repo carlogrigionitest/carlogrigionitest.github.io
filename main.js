5a2cf09051cd64cdbbecbac2bd5fca1fa09db4e7
CarloGrigioni <carlogrigioni@gmail.com>
2021-12-10 09:20:46 +0100

    default prompt

--- main.js
+++ main.js
@@ -5,29 +5,31 @@ window.onload = () => {
     navigator.serviceWorker.register('sw.js');
   }
 }
-  let deferredPrompt;
+/* let deferredPrompt;
 
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  //e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showInstallPromotion();
 
-    addBtn.addEventListener('click', () => {
-      // hide our user interface that shows our A2HS button
-      //addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', () => {
+    // hide our user interface that shows our A2HS button
+    //addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
     });
   });
+});*/
