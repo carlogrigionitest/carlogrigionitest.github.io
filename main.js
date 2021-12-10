781ab2270cefebd6679fe3b162ce96f68926e5b4
CarloGrigioni <carlogrigioni@gmail.com>
2021-12-10 21:33:37 +0100

    back to alert

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.onload = () => {
 function greet()
 {
   let name = document.querySelector('#name').value;//identify any element of HTML
-  console.log('hello, '+name);
+  alert('hello, '+name);
 }
 // codice morto per custom install
 /* let deferredPrompt;
